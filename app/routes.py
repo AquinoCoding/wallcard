a6abf8a06ea52c554ec03f53a703a6a826f51a34
Lucas Aquino <lucasaqs09@gmail.com>
2022-02-20 01:37:14 -0300

    Vesion complete imagem

--- app/routes.py
+++ app/routes.py
@@ -3,13 +3,8 @@ from flask import render_template
 from app.random.models import randomAction
 
 @app.route("/")
-def main():
-    return '<a href="/index"><button>Cartas</button></a>'
-
-@app.route("/index")
 def index():
     cartasValue = randomAction().randomize()
-    
     return render_template('index.html', cartas=cartasValue)
 
 
